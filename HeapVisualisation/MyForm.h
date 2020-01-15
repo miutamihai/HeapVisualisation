6f088cdbf85bdd15345f6b8befa53d4350afbfe3
miutamihai <miuta.mihai@gmail.com>
2020-01-14 22:29:42 -0800

    sized window and corrected base parameters

--- HeapVisualisation/MyForm.h
+++ HeapVisualisation/MyForm.h
@@ -48,11 +48,20 @@ namespace HeapVisualisation {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"MyForm";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->SuspendLayout();
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(663, 413);
+			this->MaximizeBox = false;
+			this->MinimizeBox = false;
+			this->Name = L"MyForm";
+			this->Text = L"Heap Visualisation";
+			this->WindowState = System::Windows::Forms::FormWindowState::Maximized;
+			this->ResumeLayout(false);
+
 		}
 #pragma endregion
 	};
